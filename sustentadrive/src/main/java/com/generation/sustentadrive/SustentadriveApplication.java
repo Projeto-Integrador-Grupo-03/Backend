7caf760d336da10da2d81dec8e578545df2ba019
Luiz Pascucho <luizpascucho@bol.com.br>
2024-04-01 10:30:43 -0300

    criando projeto Spring

---
+++ sustentadrive/src/main/java/com/generation/sustentadrive/SustentadriveApplication.java
@@ -0,0 +1,13 @@
+package com.generation.sustentadrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustentadriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustentadriveApplication.class, args);
+	}
+
+}
---
+++ sustentadrive/src/test/java/com/generation/sustentadrive/SustentadriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.sustentadrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustentadriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
