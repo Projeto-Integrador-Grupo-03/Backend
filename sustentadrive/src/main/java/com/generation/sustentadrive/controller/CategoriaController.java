f80085e37de01d7e142ea743329b915c6201b99e
vicfreitax <victoriafreitax@gmail.com>
2024-04-03 15:49:57 -0300

    métodos de delete, post e put

--- sustentadrive/src/main/java/com/generation/sustentadrive/controller/CategoriaController.java
+++ sustentadrive/src/main/java/com/generation/sustentadrive/controller/CategoriaController.java
@@ -2,17 +2,26 @@ package com.generation.sustentadrive.controller;
 
 import java.util.List;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.sustentadrive.model.Categoria;
 import com.generation.sustentadrive.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/categoria")
@@ -38,6 +47,30 @@ public class CategoriaController {
 	public ResponseEntity<List<Categoria>> getByNome(@PathVariable String nome){
 		return ResponseEntity.ok(categoriaRepository
 				.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(categoriaRepository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria) {
+		return categoriaRepository.findById(categoria.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
+					.body(categoriaRepository.save(categoria)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping ("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional <Categoria> categoria=categoriaRepository.findById(id);
+		
+		if(categoria.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		categoriaRepository.deleteById(id);
 	}
 	
 }
